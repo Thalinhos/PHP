b70b25895642d340e5e2afc1fdbbbbeb84c118c0
Thalisson Vieira <32040335+Thalinhos@users.noreply.github.com>
2023-11-24 14:32:46 -0300

    Update if-tal-pagina-façaIsso.php

--- Laravel/if-tal-pagina-façaIsso.php
+++ Laravel/if-tal-pagina-façaIsso.php
@@ -8,3 +8,10 @@
   @else
   <div class="card-body p-0">
   @endif
+----------- php purp abaixo -------- 
+<div class="card border-0 bg-transparent">
+  @if (str_starts_with(request()->path(), 'acompanhante/'))
+  <div class="card-body p-0 pb-5">
+  @else
+  <div class="card-body p-0">
+  @endif
